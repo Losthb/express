c4379cc95b776bced561b2302c31975499e91eb3
lovezdy <398516771@qq.com>
2018-05-22 01:10:15 +0800

    lovezdy

--- routes/httpclienturl.js
+++ routes/httpclienturl.js
@@ -4,18 +4,20 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var http = require('../bin/www');
+
 /* HTTP test listing. */
+
 router.get('/', function (req, res1, next,) {
-    const httpurl = "http://192.168.3.42:9527/get?nodejs=httclient";
+    const httpurl = `http://${http.ip1}:${http.port2}`+"/get?nodejs=httclient";
     let hbbody;
     request(httpurl, function (error, res, body, req) {
         if (!error && res.statusCode == 200) {
-           console.log(body);
-           hbbody = body;
-        }
-        res1.end(hbbody)
+            console.log(body);
+            hbbody = body;
+            }
+            res1.end(hbbody)
+        });
     });
-});
-
 
 module.exports = router;
